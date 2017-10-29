b29fa3463fd79528c9e9768c76cce24617d129ed
Keshav Taurah <mysharepointlab@gmail.com>
2017-10-29 13:42:52 +0400

    adding files

---
+++ firsttest/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="8b7be2e4-5e46-4e43-9b46-0a5130880636" featureId="8b7be2e4-5e46-4e43-9b46-0a5130880636" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="firsttest Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="90ca2706-965e-478b-9539-6822070e6ffb" />
+  </projectItems>
+</feature>
---
+++ firsttestWeb/Services/RemoteEventReceiver1.svc.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.EventReceivers;
+
+namespace firsttestWeb.Services
+{
+    public class RemoteEventReceiver1 : IRemoteEventService
+    {
+        /// <summary>
+        /// Handles events that occur before an action occurs, such as when a user adds or deletes a list item.
+        /// </summary>
+        /// <param name="properties">Holds information about the remote event.</param>
+        /// <returns>Holds information returned from the remote event.</returns>
+        public SPRemoteEventResult ProcessEvent(SPRemoteEventProperties properties)
+        {
+            SPRemoteEventResult result = new SPRemoteEventResult();
+            using (ClientContext clientContext = TokenHelper.CreateRemoteEventReceiverClientContext(properties))
+            {
+                if (clientContext != null)
+                {
+                    clientContext.Load(clientContext.Web);
+                    clientContext.ExecuteQuery();
+                }
+            }
+            return result;
+        }
+        public void ProcessOneWayEvent(SPRemoteEventProperties properties)
+        {
+            // On Item Added event, the list item creation executes
+            if (properties.EventType == SPRemoteEventType.ItemAdded)
+            {
+                using (ClientContext clientContext = TokenHelper.CreateRemoteEventReceiverClientContext(properties))
+                {
+                    if (clientContext != null)
+                    {
+                        try
+                        {
+                            clientContext.Load(clientContext.Web);
+                            clientContext.ExecuteQuery();
+                            List imageLibrary = clientContext.Web.Lists.GetByTitle("secondlist");
+                            ListItemCreationInformation itemCreateInfo = new ListItemCreationInformation();
+                            ListItem oListItem = imageLibrary.AddItem(itemCreateInfo);
+                            oListItem["Title"] = "A NEW ITEM WAS CREATED IN ANOTHER LIST";
+                            oListItem.Update();
+                            clientContext.ExecuteQuery();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Handles events that occur after an action occurs, such as after a user adds an item to a list or deletes an item from a list.
+        /// </summary>
+        /// <param name="properties">Holds information about the remote event.</param>
+
+    }
+}
